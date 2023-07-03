ee4dd79b2b94b948b76c0de68078ff7a8905a4ad
Anubhavk16 <136326324+Anubhavk16@users.noreply.github.com>
2023-07-03 18:54:58 +0530

    cors origin changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors =require('cors')
 const mongoose=require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,6 +8,11 @@ const router=require('./routes/Index')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(router)
+
+ 
+app.use(cors({
+   origin: "http://localhost:3000"
+}));
 // require('dotenv').dbconn()
 // const connectDB =require('./config/dbconn')
 // const controllers=require('./controllers/controllers');
@@ -24,6 +30,13 @@ app.use(router)
 // }
 // app.use(express.json())
 // app.use("/api",auth)
+
+// app.use((req, res, next) => {
+//    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//    next();
+//  });
  app.listen(3001,()=>{
     // connectDB();
     console.log(`server in runing  at port `)
